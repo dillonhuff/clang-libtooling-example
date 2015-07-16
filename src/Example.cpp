b33a5bcd78e2018cd3131c032b0b05dfd1580958
Dillon Huff <dillonhuff@gmail.com>
2015-07-16 13:46:05 -0500

    Added Example.cpp

---
+++ src/Example.cpp
@@ -0,0 +1,37 @@
+#include "clang/Driver/Options.h"
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/ASTConsumers.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+
+using namespace std;
+using namespace clang;
+using namespace clang::driver;
+using namespace clang::tooling;
+using namespace llvm;
+
+Rewriter rewriter;
+int numFunctions = 0;
+
+static cl::OptionCategory MyToolCategory("My tool options");
+
+int main(int argc, const char **argv) {
+  // parse the command-line args passed to your code
+  CommonOptionsParser op(argc, argv, MyToolCategory);
+  // create a new Clang Tool instance (a LibTooling environment)
+  ClangTool Tool(op.getCompilations(), op.getSourcePathList());
+
+  // run the Clang Tool, creating a new FrontendAction (explained below)
+  int result = Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
+
+  errs() << "\nFound " << numFunctions << " functions.\n\n";
+  // print out the rewritten source code ("rewriter" is a global var.)
+  //  rewriter.getEditBuffer(rewriter.getSourceMgr().getMainFileID()).write(errs());
+  return result;
+}
